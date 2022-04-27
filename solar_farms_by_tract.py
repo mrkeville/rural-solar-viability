78890b24321f6693d0a87fa48e91d5c022ce8ba8
mrkeville <98175413+mrkeville@users.noreply.github.com>
2022-04-27 08:33:21 -0400

    Create solar_farms_by_tract.py

---
+++ solar_farms_by_tract.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 08:16:09 2022
+
+@author: mrkev
+"""
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+import numpy as np
+import scipy
+
+#%%
+
