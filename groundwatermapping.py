55ab1cc2bc95ad8e34509db83bf1d6d097a22358
mrkeville <98175413+mrkeville@users.noreply.github.com>
2022-04-27 08:09:47 -0400

    Create groundwatermapping.py

---
+++ groundwatermapping.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 07:57:36 2022
+
+@author: mrkev
+"""
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+import numpy as np
+import scipy
+
+#%%
+
